70c231e97b38e9973e3f30139a0be168005e0b75
E.K.S <81411642+ErfKS@users.noreply.github.com>
2022-07-04 07:34:54 +0430

    Update installer.bat

--- installer.bat
+++ installer.bat
@@ -1,4 +1,18 @@
 @echo off
+
+echo Are you installed "Composer"?
+
+set /p Composer=
+set T=F
+if %larMix%==n set T=T
+if %larMix%==N set T=T
+if "%T%"=="T" (
+	echo Please install composer from "https://getcomposer.org/download/"
+	pause
+	exit
+)
+
+call composer global require laravel/installer
 set Files=%~dp0\Files
 set installPath=C:\Users\%username%\AppData\Roaming\Composer\vendor\bin
 copy "%Files%\create package(json).bat" "%installPath%"
